cba923df2825a4cc57636be74ad64c1748dfebc4
JoshLi <542938192@qq.com>
2019-08-14 18:16:50 +0800

    Update Android SDK to v3.3.4

---
+++ android/app/src/main/java/io/dcloud/feature/jpush/PushMessageReceiver.java
@@ -0,0 +1,7 @@
+package io.dcloud.feature.jpush;
+
+import cn.jpush.android.service.JPushMessageReceiver;
+
+public class PushMessageReceiver extends JPushMessageReceiver {
+    // 不重写方法，相应回调会转发到 JpushReceiver
+}
